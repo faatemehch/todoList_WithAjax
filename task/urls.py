b71e2b6c8b2e24869b562a656e9db0ba63a720b6
faatemeh chamani <fch199674@yahoo.com>
2021-05-23 21:58:25 +0430

    login register user

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import task_list, delete_task, complete_task
+
+from todoList_WithAjax import settings
+from .views import task_list, delete_task, complete_task, CostumeLoginView, RegisterView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path( '', task_list, name='task_list' ),
-    path( 'delete_task/<int:pk>' , delete_task, name = 'delete_task' ),
-    path( 'complete_task/<int:pk>' , complete_task, name = 'complete_task' ),
+    path( 'delete_task/<int:pk>', delete_task, name='delete_task' ),
+    path( 'complete_task/<int:pk>', complete_task, name='complete_task' ),
+    path( 'login/', CostumeLoginView.as_view(), name='login' ),
+    path( 'register/', RegisterView.as_view(), name='register' ),
+    path( 'logout/', LogoutView.as_view( next_page='login' ), name='logout' ),
 ]
--- task/views.py
+++ task/views.py
@@ -1,12 +1,17 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_exempt
-
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 from .models import Task
 from .forms import TaskForm
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 
 
+@login_required( login_url='login' )
 def task_list(request):
     tasks = Task.objects.filter( user=request.user ).all().order_by( 'date_added' )
     task_form = TaskForm( request.POST or None )
@@ -25,7 +30,7 @@ def task_list(request):
     return render( request, 'task/task_list.html', context )
 
 
-# @csrf_exempt
+@login_required( login_url='login' )
 def delete_task(request, **kwargs):
     task = Task.objects.filter( user=request.user, id=kwargs['pk'] ).first()
     if task is not None:
@@ -39,6 +44,7 @@ def delete_task(request, **kwargs):
     return render( request, 'task/task_list.html', context )
 
 
+@login_required( login_url='login' )
 def complete_task(request, **kwargs):
     task = Task.objects.filter( user=request.user, id=kwargs['pk'] ).first()
     if task is not None:
@@ -49,8 +55,39 @@ def complete_task(request, **kwargs):
         task.save()
         return JsonResponse( {'task': model_to_dict( task )}, status=200 )
 
-    # context = {
-    #     'tasks': Task.objects.filter( user=request.user ).all().order_by( 'date_added' ),
-    #     'task_form': TaskForm( request.POST or None )
-    # }
-    # return render( request, 'task/task_list.html', context )
+    context = {
+        'tasks': Task.objects.filter( user=request.user ).all().order_by( 'date_added' ),
+        'task_form': TaskForm( request.POST or None )
+    }
+    return render( request, 'task/task_list.html', context )
+
+
+class CostumeLoginView( LoginView ):
+    fields = '__all__'
+    template_name = 'auth/login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy( 'task_list' )
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect( 'login/' )
+        return super( CostumeLoginView, self ).get( *args, **kwargs )
+
+
+class RegisterView( FormView ):
+    form_class = UserCreationForm
+    template_name = 'auth/register.html'
+    success_url = reverse_lazy( 'task_list' )
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect( 'task_list' )
+        return super( RegisterView, self ).get( *args, **kwargs )
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login( self.request, user )
+        return super( RegisterView, self ).form_valid( form )
